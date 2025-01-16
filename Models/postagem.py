ce2a422bfe3836f9dafd36ab12f0fb0aa8075eba
bruunovsanttos <bruno-vieira-santos@hotmail.com>
2025-01-16 12:45:35 -0300

    criando modelo de postagem (init) na classe postagem

---
+++ Models/postagem.py
@@ -0,0 +1,10 @@
+class Postagem():
+
+    def __init__(self, id_postagem, title, content, category, tags, createdAt, updateAt):
+        self.id_postagem = id_postagem
+        self.title = title
+        self.content = content
+        self.category = category
+        self.tags = tags
+        self.createdAt = createdAt
+        self.updateAt = updateAt 
