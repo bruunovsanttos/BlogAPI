83fa30434b09eac7f0967bdd958d5d8a05d55997
bruunovsanttos <bruno-vieira-santos@hotmail.com>
2025-01-23 13:06:06 -0300

    iniciando model

--- models.py
+++ models.py
@@ -1,3 +1,17 @@
 #modleo de como deve ser a ideia do que se necessita para postagem
 #modelo de criação do banco de dados com quantidade de caracteres e primary key
 #criação tambem da def para tranformar em dict as infos
+
+from sqlalchemy import banco
+
+class PostagemModel(banco.Model):
+    pass
+
+
+    def __init__(self):
+        pass
+
+    def json(self):
+        pass
+
+    
