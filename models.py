00e76840e3e1d0c0321d334f4b4f77779a7611e3
bruunovsanttos <bruno-vieira-santos@hotmail.com>
2025-01-24 12:57:57 -0300

    refactor: utilizando o func.now para estanciar hora no banco e não na criação

--- models.py
+++ models.py
@@ -14,9 +14,9 @@ class PostagemModel(banco.Model):
     content = banco.Column(banco.String(1000))
     category = banco.Column(banco.String(100))
     tags = banco.Column(banco.String(100))
-    createdAt = banco.Column(banco.Datetime, default=datetime.now())
-    updateAt = banco.Colum(banco.Datetime, default=datetime.now())
-    
+    createdAt = banco.Column(banco.Datetime, default=func.now())
+    updateAt = banco.Colum(banco.Datetime, default=func.now(), onupdate=func.now()) #onupdate=func.now() faz com que o valor dessa coluna seja automaticamente atualizado para a data/hora atual sempre que o registro for modificado.
+
 
 
 #necessario ter id, titulo, conteudo, categoria, tags (mas pode deixar null), criado em , atualizado em.
