2e912752ae6088c13ccac95cbec07b32a658dd9c
bruunovsanttos <bruno-vieira-santos@hotmail.com>
2025-01-24 12:50:34 -0300

    feat:criando estancias de banco de dados para o programa

--- models.py
+++ models.py
@@ -3,9 +3,20 @@
 #criação tambem da def para tranformar em dict as infos
 
 from sqlalchemy import banco
+import datetime
 
 class PostagemModel(banco.Model):
-    pass
+    __tablename__ = 'postagem'
+
+
+    id_postagem = banco.Column(banco.String, primary_key=True)
+    title = banco.Column(banco.String(50))
+    content = banco.Column(banco.String(1000))
+    category = banco.Column(banco.String(100))
+    tags = banco.Column(banco.String(100))
+    createdAt = banco.Column(banco.Datetime, default=datetime.now())
+    updateAt = banco.Colum(banco.Datetime, defau)
+
 
 #necessario ter id, titulo, conteudo, categoria, tags (mas pode deixar null), criado em , atualizado em.
     def __init__(self, id_postagem, title, content, category, tags, createdAt, updateAt):
